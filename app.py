1e6dd25cd96e200d1a0eddecd2668f42a76e9989
Josh Axelman <JAxelman@oceanx.com>
2017-01-30 14:22:09 -0800

    house of reps working. Added slight error handling for non zipcodes

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ class CongressBot:
             config = yaml.load(f)
         self.db = sqlite3.connect('data/congress.db', isolation_level=None)
         self.twitter_replies(config)
+        #self.get_house_rep('90048')
         self.db.close()
 
     def get_state(self, zipcode):
         cur = self.db.cursor()
         t = (zipcode,)
         try:
-            cur.execute('SELECT state FROM zipcodes WHERE zip=?', t)
+            cur.execute("SELECT state FROM zipcodes WHERE zip=?", t)
             return cur.fetchone()[0]
         except Exception, e:
             print " - zipcode not found %s" % e
@@ -27,10 +28,26 @@ class CongressBot:
         cur = self.db.cursor()
         t = (state,)
         try:
-            cur.execute('SELECT * FROM congress WHERE state_short=?', t)
+            cur.execute("SELECT * FROM congress WHERE state_short=?", t)
             return cur.fetchall()
         except Exception, e:
             print " - state not found %s" % e
+
+    def get_house_rep(self, zip):
+        cur = self.db.cursor()
+        twitter_handles = []
+        t = (zip,)
+        cur.execute("SELECT * FROM house_rep WHERE zip=?", t)
+        reps = cur.fetchall()
+        for rep in reps:
+            #print(rep[1])
+            split_name = rep[1].split()
+            firstname = split_name[0]
+            lastname = split_name[len(split_name)-1]
+            search_name = "%" + firstname + ' ' + lastname + "%"
+            cur.execute("SELECT name, twitter_handle FROM house_twitter WHERE name like ?", (search_name, ))
+            twitter_handles.append(cur.fetchone())
+            return twitter_handles
 
     def twitter_replies(self, config):
         print (config)
@@ -51,29 +68,54 @@ class CongressBot:
                     user = tweet._json['user']['screen_name']
                     sentTweets.append(tweet.id)
                     zipcode = tweet.text.lower().replace('@congress_bot ', '')
-                    print (zipcode)
-                    print('tweet id: ' + str(tweet.id))
-                    print(tweet._json['user']['screen_name'])
-                    results = self.get_senators(self.get_state(zipcode))
-                    reply = 'Demand action from '
-                    for row in results:
-                        reply = reply + row[3]
-                        if row[4]:
-                            reply = reply + ' ' + row[4]
-                        if row[5]:
-                            reply = reply + ' ' + row[5]
-                        if row[6]:
-                            reply = reply + ' ' + row[6]
-                        reply = reply + ' '
-                    message = '@' + user + ' ' + reply
-                    if len(results) > 0:
+                    if not zipcode.isdigit():
                         try:
-                            api.update_status(message, tweet.id)
+                            message = '@' + user + ' please use 5 digit zip code only'
+                            #api.update_status(message, tweet.id)
                             print(message)
                         except Exception, e:
                             print " - failed (maybe a duplicate?): %s" % e
                     else:
-                        print('Not Found')
+                        print (zipcode)
+                        print('tweet id: ' + str(tweet.id))
+                        print(tweet._json['user']['screen_name'])
+                        results = self.get_senators(self.get_state(zipcode))
+                        reply = 'Demand action from '
+                        for row in results:
+                            reply = reply + row[3]
+                            if row[4]:
+                                reply = reply + ' ' + row[4]
+                            if row[5]:
+                                reply = reply + ' ' + row[5]
+                            if row[6]:
+                                reply = reply + ' ' + row[6]
+                            reply = reply + ' '
+                        message = '@' + user + ' ' + reply
+                        if len(results) > 0:
+                            try:
+                                #api.update_status(message, tweet.id)
+                                print(message)
+                            except Exception, e:
+                                print " - failed (maybe a duplicate?): %s" % e
+                        else:
+                            print('Not Found')
+
+                        # get the House Reps, send a second tweet
+                        twitter_handles = self.get_house_rep(zipcode)
+                        reply = 'Demand action from your Reps '
+                        for rep in twitter_handles:
+                            if rep:
+                                print(rep[0] + ' ' + rep[1])
+                                reply = reply + rep[0] + ' ' + rep[1]
+                        message = '@' + user + ' ' + reply
+                        if rep:
+                            try:
+                                #api.update_status(message, tweet.id)
+                                print(message)
+                            except Exception, e:
+                                print " - failed (maybe a duplicate?): %s" % e
+                        else:
+                            print('Not Found')
 
             time.sleep(15)
 
